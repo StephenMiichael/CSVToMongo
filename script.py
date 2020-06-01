315fb9dfaa2b7ce568a9bbcb89af91f5c7d32348
Stephen <stephenmichaela@gmail.com>
2020-06-01 17:54:39 -0300

    The script now can run in terminal. add a new library to move files in UNIX systens.

--- script.py
+++ script.py
@@ -4,6 +4,10 @@
 ##Imports
 import os #To get the current path and execute commands in terminal
 import time #To sleep/wait
+import shutil #To move files on UNIX
+from dotenv import load_dotenv #To load the .env in pyhthon.
+load_dotenv() #To load .env file
+
 
 ##Configuration
 fileExtension = '.csv' #To list/get all files with fileExtension. (Need dot, example: .csv | .txt | .docx )
@@ -18,7 +22,7 @@ def getCurrentPath():
 #Make a list of files with your fileExtension
 def getFiles(extension):
     filesList = [] #Empty list of files
-    for file in os.listdir(fr'{getCurrentPath()}\Files\Pending'): #List all files in folder
+    for file in os.listdir(fr'{getCurrentPath()}/Files/Pending'): #List all files in folder
         if file.endswith(extension): #If the extension file is equal fileExtension
             if " " in file:
                 file = replaceSpaceFile(file)
@@ -32,15 +36,15 @@ def removeDot(string):
 #Replace spaces to underline of filename
 def replaceSpaceFile(fileName):
     newFileName = fileName.replace(' ', '_') #New name of file
-    os.rename(fr'{getCurrentPath()}\Files\Pending\{fileName}', fr'{getCurrentPath()}\Files\Pending\{newFileName}') #Rename the old filename to the newFileName
+    os.rename(fr'{getCurrentPath()}/Files/Pending/{fileName}', fr'{getCurrentPath()}/Files/Pending/{newFileName}') #Rename the old filename to the newFileName
     return newFileName 
 
 ##Insert files with fileExtension to the MongoDB
 def insertMongo(files):
     for i in range(len(files)): #For with index for all files with fileExtension in folder
         print(f'Importing file: {files[i]}')
-        os.system(fr'mongoimport -h {os.getenv("server")} --port {os.getenv("port")} --db {os.getenv("databaseName")} -c={os.getenv("collectionName")} --type {removeDot(fileExtension)} --headerline --file {getCurrentPath()}\Files\Pending\{files[i]}') #Insert into MongoDB
-        os.system(fr'move {getCurrentPath()}\Files\Pending\{files[i]} {getCurrentPath()}\Files\Concluded') #Move inserted files into MongoDB to concludedFolder
+        os.system(fr'mongoimport -h {os.getenv("server")} --port {os.getenv("port")} --db {os.getenv("databaseName")} -c={os.getenv("collectionName")} --type {removeDot(fileExtension)} --headerline --file {getCurrentPath()}/Files/Pending/{files[i]}') #Insert into MongoDB
+        shutil.move(fr'{getCurrentPath()}/Files/Pending/{files[i]}', fr'{getCurrentPath()}/Files/Concluded') #Move inserted files into MongoDB to concludedFolder
 
 def main():
     while(True): #Loop the script 
